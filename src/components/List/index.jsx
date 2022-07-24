09e6d7d684d4007c2dfc12894481affd236bbcb4
steve zhang <871506263@qq.com>
2022-07-24 23:59:24 +0800

    提交代码于: 2022-07-24 23:59:24

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import listStyle from './index.module.css';
+import ListStyle from './index.module.css';
 
 class List extends Component {
   render() {
     const { nanoid } = this.props;
-    return <div className={listStyle.list}>nanoid:{nanoid}</div>;
+    return <div className={ListStyle.list}>nanoid:{nanoid}</div>;
   }
 }
 
