e4bc6a65b23999bc3e22fdd834a2d49c01d149b3
steve zhang <871506263@qq.com>
2022-07-31 09:02:28 +0800

    提交代码于: 2022-07-31 09:02:28

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ export default class App extends Component {
   render() {
     console.log('render this.props : ', this.props);
     let { children } = this.props;
-    children = [...children];
+    children = children ? [].concat(children) : [];
+    console.log('render children:', children);
     const { path } = this.state;
     return (
       <div>
@@ -23,6 +24,8 @@ export default class App extends Component {
             </div>
           </div>
         </div>
+
+        {/* <BrowserRouter> */}
         <div className='row content'>
           <div className='col-xs-2 col-xs-offset-2'>
             <div className='list-group'>
@@ -30,35 +33,33 @@ export default class App extends Component {
               {/* <a className="list-group-item" href="./about.html">About</a>
 							<a className="list-group-item active" href="./home.html">Home</a> */}
               {/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-              <BrowserRouter>
-                <Link
-                  className={path === '/home' ? 'list-group-item active' : 'list-group-item'}
-                  to='/home'
-                  onClick={() => this.handleClick('/home')}>
-                  Home
-                </Link>
-                &nbsp;&nbsp;
-                <Link
-                  className={path === '/about' ? 'list-group-item active' : 'list-group-item'}
-                  to='/about'
-                  onClick={() => this.handleClick('/about')}>
-                  About
-                </Link>
-              </BrowserRouter>
+              <Link
+                className={path === '/home' ? 'list-group-item active' : 'list-group-item'}
+                to='/home'
+                onClick={() => this.handleClick('/home')}>
+                Home
+              </Link>
+              &nbsp;&nbsp;
+              <Link
+                className={path === '/about' ? 'list-group-item active' : 'list-group-item'}
+                to='/about'
+                onClick={() => this.handleClick('/about')}>
+                About
+              </Link>
             </div>
           </div>
           <div className='col-xs-6'>
             <div className='panel'>
               <div className='panel-body'>
                 {/* 注册路由 */}
-                <BrowserRouter>
-                  <Route path='/about' component={About} />
-                  <Route path='/home' component={Home} />
-                </BrowserRouter>
+                <Route path='/about' component={About} />
+                <Route path='/home' component={Home} />
               </div>
             </div>
           </div>
         </div>
+        {/* </BrowserRouter> */}
+
         <div className='row footer'>
           {children.map((item, index) => {
             return <div key={index}>{item}</div>;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import React from 'react';
 //引入ReactDOM
 import ReactDOM from 'react-dom';
 //引入路由库
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 //引入App root组件(根组件)
 import App from './App';
 
 ReactDOM.render(
-  // <BrowserRouter>
+  <BrowserRouter>
     <App>
       <div>aaa</div>
       <div>bbb</div>
       <div>ccc</div>
     </App>
-  // </BrowserRouter>
+  </BrowserRouter>
   ,
   document.getElementById('root')
 );
