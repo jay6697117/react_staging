0aa0824b19b4482dca652af064074edc53f067a2
steve zhang <871506263@qq.com>
2022-08-03 10:08:55 +0800

    提交代码于: 2022-08-03 10:08:55

--- src/components/MyNavLink/index.jsx
+++ src/components/MyNavLink/index.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
-import {NavLink} from 'react-router-dom'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class MyNavLink extends Component {
-	render() {
-		// console.log(this.props);
-		return (
-			<NavLink activeClassName="atguigu" className="list-group-item" {...this.props}/>
-		)
-	}
+  render() {
+    console.log('MyNavLink this.props:', this.props);
+    return <NavLink activeClassName='atguigu' className='list-group-item' {...this.props} />;
+  }
 }
