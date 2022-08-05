210011265ad850d0777ee5eaef832e61b1702058
steve zhang <871506263@qq.com>
2022-08-05 23:44:02 +0800

    提交代码于: 2022-08-05 23:44:02

---
+++ 12_src_解决样式丢失问题_实践/components/MyNavLink/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class MyNavLink extends Component {
+  render() {
+    console.log('MyNavLink this.props:', this.props);
+    // const { to, children } = this.props;
+    // return (
+    //   <NavLink activeClassName='atguigu' className='list-group-item' to={to}>
+    //     {children}
+    //   </NavLink>
+    // );
+    // return <NavLink activeClassName='atguigu' className='list-group-item' to={to} children={children} />;
+    return <NavLink activeClassName='atguigu' className='list-group-item' {...this.props} />;
+  }
+}
--- src/components/MyNavLink/index.jsx
+++ src/components/MyNavLink/index.jsx
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom'
 
 export default class MyNavLink extends Component {
-  render() {
-    console.log('MyNavLink this.props:', this.props);
-    // const { to, children } = this.props;
-    // return (
-    //   <NavLink activeClassName='atguigu' className='list-group-item' to={to}>
-    //     {children}
-    //   </NavLink>
-    // );
-    // return <NavLink activeClassName='atguigu' className='list-group-item' to={to} children={children} />;
-    return <NavLink activeClassName='atguigu' className='list-group-item' {...this.props} />;
-  }
+	render() {
+		// console.log(this.props);
+		return (
+			<NavLink activeClassName="atguigu" className="list-group-item" {...this.props}/>
+		)
+	}
 }
