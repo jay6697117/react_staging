b7612cba172316f671c451ccd219440c63b99a37
steve zhang <871506263@qq.com>
2022-08-15 00:15:12 +0800

    提交代码于: 2022-08-15 00:15:12

--- config-overrides.js
+++ config-overrides.js
@@ -5,12 +5,13 @@ module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: true
+    style: 'css'
+    // style: true
   }),
-  addLessLoader({
-    lessOptions: {
-      javascriptEnabled: true,
-      modifyVars: { '@primary-color': 'green' }
-    }
-  })
+  // addLessLoader({
+  //   lessOptions: {
+  //     javascriptEnabled: true,
+  //     modifyVars: { '@primary-color': 'green' }
+  //   }
+  // })
 );
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Button, DatePicker } from 'antd';
 import { WechatOutlined, WeiboOutlined, SearchOutlined } from '@ant-design/icons';
-// import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css'; //全量引入样式
 const { RangePicker } = DatePicker;
 
 export default class App extends Component {
